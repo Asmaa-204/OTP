15ef30ff23d5c19e5e2b932c76cc8c77ff54e540
Asmaa-204 <asmaa.abozaid204@gmail.com>
2025-05-03 17:38:40 +0300

    added report

--- modules/streamCipher.py
+++ modules/streamCipher.py
@@ -11,6 +11,7 @@ class StreamCipher:
         print(f"INSIDE STREAM CIPHER: {plaintext}\n")
         keystream = self.lcg.generate(len(plaintext))
         ciphertext = bytes([p ^ k for p, k in zip(plaintext, keystream)])
+        print(f"INSIDE STREAM CIPHER: {(ciphertext)}\n")
         return ciphertext
     
     def decrypt(self, ciphertext):
